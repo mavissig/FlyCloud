419f4473c15e9f0afc26dd5e931816e2e1c858a8
Egor Kondratov <kondratov1997@gmail.com>
2023-09-15 18:41:16 +0700

    fix: data to body

--- server/authorization/models.py
+++ server/authorization/models.py
@@ -1,20 +1,24 @@
 import json
 from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 
 
+@csrf_exempt
 def authorization_user(request):
     if request.method == 'POST':
-        data = json.loads(request.data)
+        data = json.loads(request.body)
         print(data)
         try:
-            user = User.objects.get(username=data['username'])
+            user = User.objects.filter(username=data['username'])
             if user.check_password(data['password']):
-                return JsonResponse({'message': 'Authorized'}, 200)
+                response_data = {'message': 'Authorized'}
+                return JsonResponse(response_data, status=200)
             else:
-                return JsonResponse({'message': 'Unauthorized'}, 401)
+                response_data = {'message': 'Unauthorized'}
+                return JsonResponse(response_data, status=401)
         finally:
             response_data = {'error': 'User not found'}
-            return JsonResponse(response_data, 404)
+            return JsonResponse(response_data, status=404)
     else:
         pass
--- server/authorization/tests.py
+++ server/authorization/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 
+
 class AuthorizationUserTestCase(TestCase):
 
     def setUp(self):
@@ -8,13 +9,13 @@ class AuthorizationUserTestCase(TestCase):
 
     def test_authorization_success(self):
         c = Client()
-        response = c.post('auth/', {'username': 'admin', 'password': 'admin'})
+        response = c.post('', {'username': 'admin', 'password': 'admin'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json().get('message'), 'Authorized')
 
     def test_authorization_failure_wrong_password(self):
         c = Client()
-        response = c.post('auth/', {'username': 'admin', 'password': 'wrong_password'})
+        response = c.post('', {'username': 'admin', 'password': 'wrong_password'})
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json().get('message'), 'Unauthorized')
 
