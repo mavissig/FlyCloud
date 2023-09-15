206b424795002fbd46a54fdcf0666f6e8840bab5
Nerissa Sharice <nerissas@in-e8.nsk.21-school.ru>
2023-09-15 18:41:51 +0700

    change auth data, set request to django api

--- server/authorization/models.py
+++ server/authorization/models.py
@@ -1,20 +1,22 @@
 import json
 from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 
-
+@csrf_exempt
 def authorization_user(request):
     if request.method == 'POST':
-        data = json.loads(request.data)
+        data = json.loads(request.body)
         print(data)
         try:
             user = User.objects.get(username=data['username'])
+            print(f'USER={user}')
             if user.check_password(data['password']):
-                return JsonResponse({'message': 'Authorized'}, 200)
+                return JsonResponse({'message': 'Authorized'}, status=200)
             else:
-                return JsonResponse({'message': 'Unauthorized'}, 401)
+                return JsonResponse({'message': 'Unauthorized'}, status=401)
         finally:
             response_data = {'error': 'User not found'}
-            return JsonResponse(response_data, 404)
+            return JsonResponse(response_data, status=404)
     else:
         pass
