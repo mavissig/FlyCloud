fd98f2322aa9277c1f07029ad267ea31e38fe99c
melpomenna <george.bebiya@mail.ru>
2023-09-16 08:55:13 +0700

    add routes

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,28 @@
-
 import { createApp } from 'vue'
-import App from './App.vue'
-        
 
-createApp(App).mount('#app')
+import {createRouter, createWebHistory} from 'vue-router'
+
+import App from './App.vue'
+import Default from './views/Default.vue'
+import Registration from './views/Registration.vue'
+
+import Files from './views/Files.vue'
+import Home from './views/Home.vue'
+import Upload from './views/Upload.vue'
+import User from './views/User.vue'
+import FileContent from './views/FileContent.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes:[
+        {path:'/',name:'Default',component:Default},
+        {path:'/registration',name:'Registration',component:Registration},
+        {path: '/home', name:'Home',component:Home},
+        {path: '/home/user-*',name:'User',component:User},
+        {path: '/home/user-*/upload',name:'Upload',component:Upload},
+        {path: '/home/user-*/files',name:'Files',component:Files},
+        {path: '/home/user-*/files/file_content-*',name:'FileContent',component:FileContent}
+    ]
+});
+
+createApp(App).use(router).mount('#app');
---
+++ client/src/views/Default.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <ul class="default_title">
+            <li><img class="title_cloud"></li>
+            <li class="title_text">FlyCloud</li>
+            <li><button>123</button></li>
+            <li><button>123</button></li>
+            <li><button>123</button></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    background:#E2DCDC;
+}
+
+.default_title {
+    white-space: nowrap;
+}
+
+.default_title li {
+    display: inline-block;
+    margin-left: 12px;
+}
+
+.title_text {
+    width: 151px;
+    height: 43px;
+    flex-shrink: 0;
+    color: #000;
+    font-family: Raleway;
+    font-size: 35px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+}
+
+.title_cloud {
+    margin-bottom: -15px;
+    fill-opacity:inherit;
+    width: 78px;
+    height: 76px;
+    flex-shrink: 0;
+    background: url('/public/title_cloud.svg');
+    background-repeat: no-repeat;
+    background-color: lightgray 50%;
+}
+
+</style>
