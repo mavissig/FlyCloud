e0cf7fb8458ec6aa738aaf2224bbc2dbbe454945
Egor Kondratov <kondratov1997@gmail.com>
2023-09-19 10:26:21 +0700

    upd: bucket forms

--- server/users/views.py
+++ server/users/views.py
@@ -1,6 +1,7 @@
 from django.http.response import JsonResponse, HttpResponseBadRequest
 from .models import *
 from django.views.decorators.csrf import csrf_exempt
+from minio import Minio
 
 
 def session_check(request):
@@ -17,8 +18,12 @@ def upload_file(request,user_id):
         if 'file' in request.FILES:
             uploaded_file = request.FILES['file']
             bucket_name = User.objects.filter(id=user_id)
-            print(bucket_name)
+            print(f'type= {type(str(bucket_name))}')
             create_minio_bucket(bucket_name)
+            minio_client = Minio("http://localhost:9001/api/v1/service-account-credentials",
+                                 access_key="0geNc5DmznJsNtMrW7MS",
+                                 secret_key="fQD2yzPGEDyUv1DoWQjMEXGMo0n0bo0CPPcmHkLX",
+                                 secure=False)
             return JsonResponse({"message": "Success Upload"}, status=200)
         else:
             return HttpResponseBadRequest("File not found in the request")
